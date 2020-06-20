50d8560306f306713f6316f5f7b65862e3b90529
Rodrigo Viturino <viturino.souza1@outlook.com>
2020-06-20 18:25:05 -0300

    adicionando modulos do express para trabalhar com rotas e criando algumas rotas de testes

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+let app = express();
+
+app.get("/", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Hello World!</h1>");
+});
+
+app.get("/users", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.json({
+    users: [
+      {
+        name: "Rodrigo",
+        email: "viturino_souza@outlook.com",
+        id: 1,
+      },
+    ],
+  });
+});
+
+app.listen(3000, "127.0.0.1", () => {
+  console.log("servidor rodando");
+});
